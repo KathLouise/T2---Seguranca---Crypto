899d1012f6130b71776ff13257a9ad5fe94ace7f
Katheryne Louise Graf <klg12@inf.ufpr.br>
2016-09-30 18:50:41 -0300

    wordlist esta funcionando, talvez precise rever o parse das strings

--- wordlist.c
+++ wordlist.c
@@ -9,12 +9,59 @@
 #include <ctype.h>
 
 #define TAM_NAMEF 256
-#define TAM_HASH 2048
+#define TAM_HASH 30000
 
 struct dictionary{
     char *word;
-    char *val;
-    struct dictionary *nest;
+    struct dictionary *next;
+};
+
+static struct dictionary *hash[TAM_HASH];
+
+unsigned value(char *word){
+    unsigned hashVal;
+    
+    for(hashVal = 0; *word != '\0'; word++){
+        hashVal = *word + (31 * hashVal);
+    }
+    return (hashVal % TAM_HASH);
+}
+
+int search(char *word){
+    struct dictionary *temp;
+    
+    for(temp = hash[value(word)]; temp != NULL; temp = temp->next){
+        if(strcmp(word, temp->word) == 0){
+            return 1;         
+        }
+    }
+    return 0;
+}
+
+struct dictionary *add(char *word){
+    struct dictionary *temp;
+    unsigned hashVal;
+
+    temp = (struct dictionary *) malloc(sizeof(*temp));
+    if(temp == NULL || (temp->word = strdup(word)) == NULL)
+        return NULL;
+    hashVal = value(word);
+    temp->next = hash[hashVal];
+    hash[hashVal] = temp;
+
+    return temp;
+}
+
+void particionate(char *word,  char *wordS){
+    int i, c = 0;
+    
+    for(i = 0; i < strlen(word); i++){
+        if(!((word[i] >=32) && (word[i] <= 64) || (word[i] >= 123) && (word[i] <= 126))){ 
+            wordS[c] = word[i];
+            c++;
+        }
+    }
+    wordS[c] = '\0';
 }
 
 void main(int argc, char *argv[]){
@@ -23,7 +70,9 @@ void main(int argc, char *argv[]){
     char str[TAM_NAMEF];
     struct dirent *dir_files;
     FILE *entry, *output;
-    struct dictionary *dic[HASHSIZE];
+    struct dictionary *dic;
+    char word[20];
+    char wordF[20];
 
     if(argc < 2){
         printf("Entrada incorreta.\n\n");
@@ -62,7 +111,15 @@ void main(int argc, char *argv[]){
             exit(0);
         }
 
-        printf("Abri o arquivo =)\n");
+        while(!feof(entry)){
+            fscanf(entry, "%s", word);
+            particionate(word,  wordF);
+                       
+            if(dic == NULL || search(wordF) == 0){
+                dic = add(wordF);
+                fprintf(output, "%s\n", wordF);
+            }
+        }
         fclose(entry);
     }    
     fclose(output);
